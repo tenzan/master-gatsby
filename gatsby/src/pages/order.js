ca5be413f5eb2cf72b644ffb48267059919ab4b0
Askar Karasaev <askar.karasaev@gmail.com>
2020-09-29 16:23:54 +0900

    lesson 5 Routing and Navigation in Gatsby

--- gatsby/src/pages/order.js
+++ gatsby/src/pages/order.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Nav from '../components/Nav';
 
 export default function OrdersPage() {
   return (
     <div>
-      <p>Hey! I'm the Orders page</p>
+      <Nav />
+      <p>Hey! I'm the Order page</p>
     </div>
   );
 }
