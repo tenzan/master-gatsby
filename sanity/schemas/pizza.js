96e11d9c376e45af2170782001fc96f96efa2e14
Askar Karasaev <askar.karasaev@gmail.com>
2020-10-02 18:26:58 +0900

    Creating Data Relationships

--- sanity/schemas/pizza.js
+++ sanity/schemas/pizza.js
@@ -37,5 +37,31 @@ export default {
       validation: (Rule) => Rule.min(1000).max(50000),
       // TODO: Add custom input component
     },
+    {
+      name: 'toppings',
+      title: 'Toppings',
+      type: 'array',
+      of: [{ type: 'reference', to: [{ type: 'topping' }] }],
+    },
   ],
+  preview: {
+    select: {
+      title: 'name',
+      media: 'image',
+      topping0: 'toppings.0.name',
+      topping1: 'toppings.1.name',
+      topping2: 'toppings.2.name',
+      topping3: 'toppings.3.name',
+    },
+    prepare: ({ title, media, ...toppings }) => {
+      // 1. Filter undefined toppings out
+      // 2. return the preview ogject for the pizza
+      const tops = Object.values(toppings).filter(Boolean);
+      return {
+        title,
+        media,
+        subtitle: tops.join(', '),
+      };
+    },
+  },
 };
