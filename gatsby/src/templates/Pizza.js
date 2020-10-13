ed8fa1178fdd4dc1e9ef7de06bb6b9a9c5d1e77c
Askar Karasaev <askar.karasaev@gmail.com>
2020-10-13 16:24:24 +0900

    lesson 24 Templating and Styling our Single Pizza Page

--- gatsby/src/templates/Pizza.js
+++ gatsby/src/templates/Pizza.js
@@ -1,8 +1,29 @@
 import { graphql } from 'gatsby';
 import React from 'react';
+import Img from 'gatsby-image';
+import styled from 'styled-components';
+import PizzasPage from '../pages/pizzas';
 
-export default function SinglePizzaPage() {
-  return <p>Single Pizza!!!</p>;
+const PizzaGrid = styled.div`
+  display: grid;
+  grid-gap: 2rem;
+  grid-template-columns: repeat(auto-fill, minmax(400px, 1fr));
+`;
+
+export default function SinglePizzaPage({ data: { pizza } }) {
+  return (
+    <PizzaGrid>
+      <Img fluid={pizza.image.asset.fluid} />
+      <div>
+        <h2 className="mark">{pizza.name}</h2>
+        <ul>
+          {pizza.toppings.map((topping) => (
+            <li key={topping.id}>{topping.name}</li>
+          ))}
+        </ul>
+      </div>
+    </PizzaGrid>
+  );
 }
 
 // This needs to be dynamic based on the slug passed in via context in gatsby-node.js
